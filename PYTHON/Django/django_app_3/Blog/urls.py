a0788f8ec02af75da9a3c8c09c5a562d3c5a56e7
Stelut Tomoiaga <stemitomy@gmail.com>
2024-02-25 02:21:40 +0100

    creada categorias.html, la vista correspondiente y registrar la la url para que django pueda visitarla

--- PYTHON/Django/django_app_3/Blog/urls.py
+++ PYTHON/Django/django_app_3/Blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.blog, name = "Blog"),
+    path('categoria/<int:categoria_id>/', views.categoria, name = "Categoria"), # categoria_id hace referencia al id en models de la categoria
 ]
--- PYTHON/Django/django_app_3/Blog/views.py
+++ PYTHON/Django/django_app_3/Blog/views.py
@@ -5,3 +5,8 @@ from Blog.models import Categoria, Post
 def blog(request):
     posts = (Post.objects.all())
     return render(request, "Blog/blog.html", {"posts": posts})
+
+def categoria(request, categoria_id):
+    categoria = (Categoria.objects.get(id = categoria_id))
+    posts = (Post.objects.filter(categorias = categoria))
+    return render(request, "Blog/categorias.html", {"categoria": categoria, "posts": posts})
