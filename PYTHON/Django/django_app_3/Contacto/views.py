baa0638febc0c4acdec8e4b0e1e631aed9117f91
Stelut Tomoiaga <stemitomy@gmail.com>
2024-02-25 04:09:07 +0100

    los datos ya se envian a mi email desde mi email

--- PYTHON/Django/django_app_3/Contacto/views.py
+++ PYTHON/Django/django_app_3/Contacto/views.py
@@ -1,17 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import FormularioContacto
 from django.http import HttpResponseRedirect
+from django.core.mail import EmailMessage
 
 # Create your views here.
 def contacto(request):
     form = FormularioContacto(request.POST)
-    # check whether it's valid:
-    if form.is_valid():
-        # process the data in form.cleaned_data as required
-        # ...
-        # redirect to a new URL:
-        return HttpResponseRedirect("/thanks/")
-     # if a GET (or any other method) we'll create a blank form
-    else:
-        form = FormularioContacto()
+    
+    if request.method == "POST":
+        formset = FormularioContacto(data = request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            nombre = request.POST.get("nombre")
+            email = request.POST.get("email")
+            contenido = request.POST.get("contenido")
+            # process the data in form.cleaned_data as required
+            nombre2 = form.cleaned_data.get("nombre")
+            
+            email = EmailMessage("Mensaje desde App Django", "El usuario con nombre {} con la dirección {} escribe lo siguiente:\n\n{}".format(nombre, email, contenido),
+                                 "", ["estemitomy@gmail.com"], reply_to = [email])
+            
+            try:
+                email.send()
+                # return redirect("/contacto/?valido")
+            except:
+                # return redirect("/contacto/?invalido")
+                pass
+            # ...
+            # redirect to a new URL:
+        # if a GET (or any other method) we'll create a blank form
+        else:
+            form = FormularioContacto()
     return render(request, "Contacto/contacto.html", {"form": form})
