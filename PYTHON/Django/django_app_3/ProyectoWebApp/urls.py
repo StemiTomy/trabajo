c021b6727d23ab10e0b7e16e5e2833e63326e6c3
Stelut Tomoiaga <stemitomy@gmail.com>
2024-02-25 01:42:32 +0100

    agregado las vistas a los posts, para que podamos ver los modelos, las vistas

--- PYTHON/Django/django_app_3/ProyectoWebApp/urls.py
+++ PYTHON/Django/django_app_3/ProyectoWebApp/urls.py
@@ -4,6 +4,5 @@ from ProyectoWebApp import views
 urlpatterns = [
     path('', views.home, name="Home"),
     path('tienda/', views.tienda, name="Tienda"),
-    path('blog/', views.blog, name="Blog"),
     path('contacto/', views.contacto, name="Contacto"),
 ]
--- PYTHON/Django/django_app_3/django_app_3/urls.py
+++ PYTHON/Django/django_app_3/django_app_3/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ProyectoWebApp.urls')),
     path('servicios/', include('Servicios.urls')),
+    path('blog/', include('Blog.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
