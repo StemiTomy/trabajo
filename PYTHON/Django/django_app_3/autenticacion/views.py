dbb1be65c86beebf8d2932fc4329d8ef66059487
Stelut Tomoiaga <stemitomy@gmail.com>
2024-02-25 20:17:35 +0100

    agregada lógica autenticacion

--- PYTHON/Django/django_app_3/autenticacion/views.py
+++ PYTHON/Django/django_app_3/autenticacion/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
 class VRegitro(View):
@@ -9,4 +12,13 @@ class VRegitro(View):
         return render(request, "registro/registro.html", {"form":form})
     
     def post(self, request):
-        pass
+        form = UserCreationForm(request.POST) # ususario y password que viene desde el form
+        if form.is_valid():
+            usuario = form.save() # guardamos la información aquí, esto hace que se guarde automáticamente en la tabla auth_user
+            # ahora queremos hacer un login
+            login(request, usuario)
+            return redirect(reverse_lazy('Home'))
+        else:
+            for msg in form.error_messages:
+                messages.error(request, form.error_messages[msg])
+            return render(request, "registro/registro.html", {"form": form})
